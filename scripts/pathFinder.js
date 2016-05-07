737b85d64ada2f839afe1051452490f03bd3a9d0
TripWireZa <tripwire.rsa@gmail.com>
2016-05-07 10:35:51 +0200

    Added knockout and bootstrap

--- scripts/pathFinder.js
+++ scripts/pathFinder.js
@@ -1,14 +1,22 @@
 function PathFinder(options) {
     var self = this;
     
+    self.NumberOfCities = options.numberOfCities;
+    self.GenerateCircle = options.generateCircle;
+    
     self.renderer = new Renderer({ 
         CityIconSize: options.CityIconSize
     });
-    self.cityManager = new CityManager(options.NumberOfCities);
+    self.cityManager = new CityManager();
     
     self.Init = function () {
         
-        self.cityManager.GenerateCities(options.GenerateCircle);
-        self.renderer.Render(self.cityManager.cities);
+        self.Reset();
+        
     };
+    
+    self.Reset = function () {
+        self.cityManager.GenerateCities(self.NumberOfCities, self.GenerateCircle);
+        self.renderer.Render(self.cityManager.cities);
+    }
 }
--- scripts/viewmodels/cityManager.js
+++ scripts/viewmodels/cityManager.js
@@ -1,23 +1,22 @@
-function CityManager(numberOfCities) {
+function CityManager() {
     var self = this;
     
-    self.numberOfCities = numberOfCities;
     self.cities = [];
     
-    self.GenerateCities = function (generateCircle) {
+    self.GenerateCities = function (numberOfCities, generateCircle) {
         if(generateCircle)
-            self.GenerateCitiesCircle();
+            self.GenerateCitiesCircle(numberOfCities);
         console.log(self.cities);
     };
     
-    self.GenerateCitiesCircle = function () {
+    self.GenerateCitiesCircle = function (numberOfCities) {
         var circleRadius = 280;
         var circleCenterX = 400;
         var circleCenterY = 300;
         
-        var radiansPerCity = (Math.PI * 2) / self.numberOfCities;
+        var radiansPerCity = (Math.PI * 2) / numberOfCities;
         
-        for (var i = 0; i < self.numberOfCities; i++)
+        for (var i = 0; i < numberOfCities; i++)
         {
             var pointX = parseInt((Math.cos(radiansPerCity * i) * circleRadius) + circleCenterX);
             var pointY = parseInt((Math.sin(radiansPerCity * i) * circleRadius) + circleCenterY);
